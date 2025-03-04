57e7b9a936c1204c7cbe4b6ad5eb41841b13a453
Ajay Bhawariya <47675480+AJAYBHAWARIYA@users.noreply.github.com>
2025-03-04 11:15:11 -0800

    Middleware setup
    - added middleware cors

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+from fastapi.middleware.cors import CORSMiddleware
 from typing import Union
 from fastapi import FastAPI
 from app.routers import patients
@@ -6,6 +7,13 @@ app = FastAPI()
 
 app.include_router(patients.router)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 @app.get("/")
 def read_root():
