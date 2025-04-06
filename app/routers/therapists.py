609ea155fba540a8c04037bb20f42cab57e9563c
21tnguyen <21tnguyen10@gmail.com>
2025-04-06 12:53:28 -0700

    create exercise post request
    - upload videos with exercise id
    - incorporate AWS, S3

--- app/routers/therapists.py
+++ app/routers/therapists.py
@@ -2,11 +2,27 @@ from fastapi import HTTPException, APIRouter
 from app.database import get_database
 from app.models.therapists import Therapist
 from pymongo.errors import PyMongoError
+from bson import ObjectId
+
+from dotenv import load_dotenv
+import os
+import boto3 
 
 collection = get_database()["Therapists"]
+routineCollection = get_database()["Routines"]
+exerciseCollection = get_database()["Exercises"]
 
 router = APIRouter(prefix="/therapist", tags=["Therapists"])
 
+
+AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_KEY = os.getenv("AWS_SECRET_KEY_ID")
+S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
+S3_REGION = os.getenv("AWS_REGION")
+
+s3_client = boto3.client(
+    "s3"
+)
 
 @router.post("/create_therapist", response_model=str, status_code=201)
 def create_new_therapist(user: Therapist):
@@ -62,3 +78,30 @@ def update_patient_by_id(therapist_username: str, user: Therapist):
     except PyMongoError as e:
         raise HTTPException(status_code=500, detail="Database update failed")
     
+
+@router.post("/upload_custom_video/")
+def upload_custom_video(exercise_id: str, filename: str):
+    try:
+        exercise = exerciseCollection.find_one({"id": ObjectId(exercise_id)})
+        if exercise:
+            s3_key = f"custom_videos/{filename}"
+
+            presigned_url = s3_client.generate_presigned_url(
+                "put_object",
+                Params={"Bucket": S3_BUCKET_NAME, "Key": s3_key, "ContentType": "video/mp4"},
+                ExpiresIn=600
+            )
+
+            video_url = f"https://{S3_BUCKET_NAME}.s3.{S3_REGION}.amazonaws.com/{s3_key}"
+
+            exerciseCollection.update_one(
+                {"_id": ObjectId(exercise_id)},
+                {"$set": {"video_url": video_url}}
+            )
+            return {"presigned_url": presigned_url, "video_url": video_url}
+
+        else:
+            # Item not found
+            raise HTTPException(status_code=404, detail="Exercise not found")
+    except PyMongoError as e:
+        raise HTTPException(status_code=500, detail="Database update failed")
